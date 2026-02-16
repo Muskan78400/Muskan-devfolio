da7554bcc4eab9c7596ed9f950e6b9e62c38dd12
Muskan <muskansingal113@gmail.com>
2026-02-16 23:22:02 +0530

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Muskan-Portfolio-react-vite/', 
+  base: '/Muskan-devfolio/', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
